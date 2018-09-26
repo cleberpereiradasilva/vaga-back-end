e6664b112ea982a6afb5e2a55ca1440873383490
noct <noct@debian.quarto.o>
2018-09-26 02:24:36 -0300

    Primeiro commit, Dockerfile

---
+++ conf/start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# ----------------------------------------------------------------------
+# Create the .env file if it does not exist.
+# ----------------------------------------------------------------------
+
+if [[ ! -f "/var/www/.env" ]] && [[ -f "/var/www/.env.example" ]];
+then
+cp /var/www/.env.example /var/www/.env
+fi
+
+# ----------------------------------------------------------------------
+# Run Composer
+# ----------------------------------------------------------------------
+
+if [[ ! -d "/var/www/vendor" ]];
+then
+rm -rf /var/www/vendor
+cd /var/www
+composer update
+composer dump-autoload -o
+chown nginx:nginx /var/www/ -R
+fi
+
+# ----------------------------------------------------------------------
+# Start supervisord
+# ----------------------------------------------------------------------
+
+exec /usr/bin/supervisord -n -c /etc/supervisord.conf
